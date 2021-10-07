26993dd9c9437a862625f6fd5bc2f40a826c9c5f
Roman Koshelev <Leo_dialog@mail.ru>
2021-10-07 13:49:43 +0300

    TASK-1 - Fixes by revue 1

--- src/main/java/org/javaSpring/task1/Anagram.java
+++ src/main/java/org/javaSpring/task1/Anagram.java
@@ -9,45 +9,47 @@ public class Anagram implements Function<String, String> {
 
     @Override
     public String apply(String s) {
-        String[] stringSeparatedByDelimiter = s.split(DELIMITER);
+        String[] words = s.split(DELIMITER);
 
-        StringJoiner reverseSL = new StringJoiner(" ");
-        for (String word: stringSeparatedByDelimiter) {
-            reverseSL.add(someLogic(word));
+        StringJoiner result = new StringJoiner(" ");
+        for (String word : words) {
+            result.add(someLogic(word));
         }
-        return reverseSL.toString();
+        return result.toString();
     }
 
-    private String reverseWord(String word){
+    private String reverse(String word) {
         StringBuilder sb = new StringBuilder(word);
         return sb.reverse().toString();
     }
 
-    private String getOnlyLetters(String word){
+    private String filterLetters(String word) {
         StringBuilder sb = new StringBuilder();
         char[] reversWordCharArr = word.toCharArray();
 
         for (int x = 0; x < reversWordCharArr.length; x++) {
-            if (Character.isLetter(reversWordCharArr[x])){
+            if (Character.isLetter(reversWordCharArr[x])) {
                 sb.append(reversWordCharArr[x]);
             }
         }
         return sb.toString();
     }
 
+    //reverse all the words of input word
+    //All non-letter symbols stay on the same places
     private String someLogic(String word) {
         StringBuilder sb = new StringBuilder();
-        char[] wordCharsArr = word.toCharArray();
-        char[] reversCharsArr = getOnlyLetters(reverseWord(word)).toCharArray();
+        char[] originalChars = word.toCharArray();
+        char[] reverslettersOnly = filterLetters(reverse(word)).toCharArray();
         int indexOfReversLetter = 0;
 
-        for(int x = 0; x < wordCharsArr.length; x++){
-            if (!Character.isLetter(wordCharsArr[x])){
-                //add only NOT letters
-                sb.append(wordCharsArr[x]);
+        for (int x = 0; x < originalChars.length; x++) {
+            if (!Character.isLetter(originalChars[x])) {
+                // add only NOT letters
+                sb.append(originalChars[x]);
             } else {
-                //add only letters
-                sb.append(reversCharsArr[indexOfReversLetter]);
+                // add only letters
+                sb.append(reverslettersOnly[indexOfReversLetter]);
                 indexOfReversLetter++;
             }
         }
